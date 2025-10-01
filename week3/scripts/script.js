b863fd25b7c33e4817d9849d9fe29a65c377b570
Tobalated <ooni@inside.artcenter.edu>
2025-09-30 22:52:33 -0400

    Updated

---
+++ week3/scripts/script.js
@@ -0,0 +1,14 @@
+let cStage = document.getElementById("colorStage")
+let cButton = document.getElementById("colorButton")
+
+let changeColor = function()
+{
+
+let rComp = Math.random() * 255
+let gComp = Math.random() * 255
+let bComp = Math.random() * 255
+
+cStage.style.backgroundColor = "rgb(" + rComp + ", " + gComp + ", " + bComp + ")"
+}
+
+cButton.addEventListener("click", changeColor)
